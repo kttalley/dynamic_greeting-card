ddd0c112d70f0fa4988add652858ff3480e58dd6
ktt <talleykristian@gmail.com>
2019-07-20 19:35:27 -0700

    timestamp successful

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,15 @@
+const today = new Date();
+const hourNow = today.getHours();
+let greeting;
 
-$(document).ready(function(){
-    
-    $(".card").on("click", function(){
-      $(".cardJaw .content").css({"max-height":"500px"});
-    });
-  });
+
+if (hourNow > 18) {
+    greeting = 'Good evening.';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon.';
+} else if (hourNow > 0) {
+    greeting = 'Good morn.';
+}
+
+document.write('<h3>' + greeting + '</h3>');
+document.write('<h4>The time is ' + hourNow + ':00</h4>');
