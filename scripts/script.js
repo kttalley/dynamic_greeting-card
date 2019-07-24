387b5775b407c34430a21d055e905118c558e087
kttalley <talleykristian@gmail.com>
2019-07-24 12:57:23 -0700

    working with event listeners

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,28 @@ if (hourNow > 18) {
 
 document.write('<h3>' + greeting + '</h3>');
 document.write('<h4>The time is ' + hourNow + ':00</h4>');
+
+//adding animation event listeners
+let cardElement = document.getElementById('card');
+
+cardElement.addEventListener('animationStart', listener, flase);
+cardElement.addEventListener('animationEnd', listener, flase);
+cardElement.addEventListener('animationIteration', listener, flase);
+
+cardElement.className = 'slideIn';
+
+function listener(event){
+    let l = document.createElement('li');
+    switch(event.type) {
+        case 'animationStart':
+            l.innerHtml = 'started: elapsed time is' + event.elapsedTime;
+            break;
+        case 'animationEnd':
+            l.innerHTML = 'Ended: elapsed time is' + event.elapsedTime;
+            break;
+        case 'animationIteration':
+            l.innerHTML = 'New loop started at time' + event.elapsedTime;
+            break;
+    }
+    document.getElementById('output').appendChild(l);
+}
